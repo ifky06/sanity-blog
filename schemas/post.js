6c59e7350e6af3cc613c9caf17b5322e548017a7
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-07-04 18:43:27 +0700

    initial commit

---
+++ schemas/post.js
@@ -0,0 +1,69 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required().max(200)
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 250
+      }
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: [{type: 'author'}],
+      validation: (Rule) => Rule.required()
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{type: 'category'}]
+    },
+    {
+      name: 'thumbnail',
+      title: 'Thumbnail',
+      type: 'image',
+      options: {
+        hotspot: true
+      },
+      validation: (Rule) => Rule.required()
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {type: 'block'},
+        {type: 'image', options: {hotspot: true}}
+      ],
+      validation: (Rule) => Rule.required()
+    },
+    {
+      name: 'featured',
+      title: 'Featured',
+      type: 'boolean',
+      description: 'Is this a featured post?',
+      initialValue: false,
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+      initialValue: new Date().toISOString(),
+      hidden: true
+    }
+  ]
+}
